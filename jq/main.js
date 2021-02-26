1ffaea4a11dffe4bd2f8f6f3649821bbc1cca122
Anthony Nolan <anthonynolan@gmail.com>
2021-02-26 18:22:44 +0000

    Lookup dicts dumped from py and loaded in js

--- jq/main.js
+++ jq/main.js
@@ -1,32 +1,67 @@
-$(document).ready(function() {
-	console.log('ready');
+$(document).ready(function () {
+	
+	var ctx;
+	
 	$('textarea').on('keydown', textEntered);
-	$('#loadModel').on('click', loadModel);
+	
+	loadModel().then((results) => { 
+		ctx = results;
+		console.log('ready to predict') 
+		console.log(tf.argMax(ctx.model.predict(tf.tensor2d([1,2,3,4,5,6,7,8,9,0,11], shape=[1,11])).arraySync()[0]).arraySync());
+	});
 });
 
-const contextSize = 2;
+const contextSize = 5;
 let wordsTagged = [];
 
-function textEntered(){
+function textEntered() {
 	let words = this.value.split(' ');
+
+	if (words.length >= contextSize) {
+		//Create the windowed dataset of width contextSize
+		//Encode each of the 11 input features from each window to produce a new array
+
+		//const features = []
+		// features.append(pos_to_index[row[1][1]])
+		// features.append(pos_to_index[row[0][1]])
+		// features.append(word_to_index[row[2][0][-3:]])
+		// features.append(word_to_index[row[2][0][0]])
+		// features.append(word_to_index[row[2][0]])
+		// features.append(word_to_index[row[1][0]])
+		// features.append(word_to_index[row[1][0][-3:]])
+		// features.append(word_to_index[row[0][0]])
+		// features.append(word_to_index[row[3][0]])
+		// features.append(word_to_index[row[3][0][-3:]])
+		// features.append(word_to_index[row[4][0]])
+		// features.append(pos_to_index[row[2][1]])
+
+
+		//Run predict with each of the arrays
+		//decode the responses and output the pairs.
+
+		wordsTagged = [];
+		for (word of words) {
+			wordsTagged[wordsTagged.length] = { "POS": "POS", "word": word };
+		}
+		console.log(wordsTagged);
 	
-	if (words.length>=contextSize){		
-		tagWords(words);
 	}
 }
 
-function tagWords(words){
-	wordsTagged = [];
-	for (word of words){
-		wordsTagged[wordsTagged.length] = {word: "POS"};
-	}
-	console.log(wordsTagged);
+async function loadModel() {
+	var results = {};
+
+	$.getJSON('word_to_index.json', (data) => {
+		results.wordToIndex = data;
+	});
+
+	$.getJSON('pos_to_index.json', (data) => {
+		results.posToIndex = data;
+	});
+
+	results.model = await tf.loadGraphModel('http://127.0.0.1:8887/tfjs_model/model.json');
+
+	return results;
 }
 
-async function loadModel(){
 	
-	const model = await tf.loadGraphModel('http://127.0.0.1:8887/tfjs_model/model.json');
-	console.log(model);
-	model.execute(tf.tensor2d([1,2,3], shape=[1,3]))
-
-}
