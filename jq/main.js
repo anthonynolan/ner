b4efcf939ff595606df990dc9c3a7136f0863f68
Anthony Nolan <anthonynolan@gmail.com>
2021-02-28 10:08:05 +0000

    Creating the windowed dataset in js

--- jq/main.js
+++ jq/main.js
@@ -1,8 +1,12 @@
+var ctx;
+
 $(document).ready(function () {
 	
-	var ctx;
+	
 	
 	$('textarea').on('keydown', textEntered);
+	
+		
 	
 	loadModel().then((results) => { 
 		ctx = results;
@@ -14,26 +18,49 @@ $(document).ready(function () {
 const contextSize = 5;
 let wordsTagged = [];
 
+
+function createWindowedDataset(data){
+	let windowed = [];
+	const sequenceLength = 5;
+	for(let i = 0; i< data.length-sequenceLength ; i++){
+		windowed[i]=data.slice(i, i+sequenceLength);
+	}
+	return windowed;
+}
+
 function textEntered() {
 	let words = this.value.split(' ');
 
 	if (words.length >= contextSize) {
 		//Create the windowed dataset of width contextSize
+		windowed = createWindowedDataset(words);
+		console.log(windowed);
 		//Encode each of the 11 input features from each window to produce a new array
+		tags = []
+		for (row of windowed){
+			const features = [];
+			//features.append(pos_to_index[row[1][1]])
+			features[features.length] = 0;
+			//features.append(pos_to_index[row[0][1]])
+			features[features.length] = 0;
+			//features.append(word_to_index[row[2][0][-3:]])
+			features[features.length] = ctx.wordToIndex[row[2].slice(-3,)];
 
-		//const features = []
-		// features.append(pos_to_index[row[1][1]])
-		// features.append(pos_to_index[row[0][1]])
-		// features.append(word_to_index[row[2][0][-3:]])
-		// features.append(word_to_index[row[2][0][0]])
-		// features.append(word_to_index[row[2][0]])
-		// features.append(word_to_index[row[1][0]])
-		// features.append(word_to_index[row[1][0][-3:]])
-		// features.append(word_to_index[row[0][0]])
-		// features.append(word_to_index[row[3][0]])
-		// features.append(word_to_index[row[3][0][-3:]])
-		// features.append(word_to_index[row[4][0]])
-		// features.append(pos_to_index[row[2][1]])
+			// features.append(word_to_index[row[2][0][0]])
+			// features.append(word_to_index[row[2][0]])
+			// features.append(word_to_index[row[1][0]])
+			// features.append(word_to_index[row[1][0][-3:]])
+			// features.append(word_to_index[row[0][0]])
+			// features.append(word_to_index[row[3][0]])
+			// features.append(word_to_index[row[3][0][-3:]])
+			// features.append(word_to_index[row[4][0]])
+			// features.append(pos_to_index[row[2][1]])
+			console.log(features);
+			features.length = 11;
+			features.fill(0, features.length);
+			tags[tags.length] = tf.argMax(ctx.model.predict(tf.tensor2d(features, shape=[1,11])).arraySync()[0]).arraySync();
+
+		}
 
 
 		//Run predict with each of the arrays
