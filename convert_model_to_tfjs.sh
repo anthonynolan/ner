bf76abc9719baa6543a34da9ebbf2e6dd2e862a0
Anthony Nolan <anthonynolan@gmail.com>
2021-03-09 11:34:14 +0000

    Working first pass sentiment analysis

--- convert_model_to_tfjs.sh
+++ convert_model_to_tfjs.sh
@@ -1,1 +1,2 @@
-tensorflowjs_converter data/model jq/tfjs_model --input_format=tf_saved_model
+tensorflowjs_converter data/model/pos_checkpoint jq/tfjs_pos_model --input_format=tf_saved_model
+tensorflowjs_converter data/model/ner_checkpoint jq/tfjs_ner_model --input_format=tf_saved_model
